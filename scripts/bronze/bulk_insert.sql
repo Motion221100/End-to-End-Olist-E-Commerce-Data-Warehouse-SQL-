3bc16a66180dd854a28e83f8b17ff63266d994e7
Motion221100 <83600286+Motion221100@users.noreply.github.com>
2026-01-18 20:47:50 +0200

    Add files via upload

---
+++ scripts/bronze/bulk_insert.sql
@@ -0,0 +1,72 @@
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/GEO/olist_geolocation_dataset.csv'
+IGNORE
+INTO TABLE bronze.geo_geolocation
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES; 
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/CUST/olist_customers_dataset.csv'
+IGNORE
+INTO TABLE bronze.cust_customers
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;   
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ORD/olist_orders_dataset.csv'
+IGNORE
+INTO TABLE bronze.ord_orders
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;   
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/SELR/olist_sellers_dataset.csv'
+IGNORE
+INTO TABLE bronze.selr_sellers
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;  
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ORD/olist_order_items_dataset.csv'
+IGNORE
+INTO TABLE bronze.ord_order_items
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;  
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ORD/olist_order_payments_dataset.csv'
+IGNORE
+INTO TABLE bronze.ord_order_payments
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;   
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/ORD/olist_order_reviews_dataset.csv'
+IGNORE
+INTO TABLE bronze.ord_order_reviews
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;   
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/PROD/olist_products_dataset.csv'
+IGNORE
+INTO TABLE bronze.prod_products
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;   
+
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 8.0/Uploads/PROD/product_category_name_translation.csv'
+IGNORE
+INTO TABLE bronze.prod_category_name_translation
+FIELDS TERMINATED BY ',' 
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES;   
